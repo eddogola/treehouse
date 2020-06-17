3f7ecd51a6f86132c9c235788c304d59a6771842
Eddy Ogola <edogola01@gmail.com>
2020-06-17 17:59:25 +0300

    booklist view url config

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.views.generic import TemplateView
 from django.urls import path
 
+from main import views
+
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home')
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('books/', views.BookListView.as_view(), name='book_list'),
 ]
