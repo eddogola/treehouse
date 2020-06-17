7ec9ce6ac823510bd7c080e42b906a05999ca023
Eddy Ogola <edogola01@gmail.com>
2020-06-17 19:24:09 +0300

    book-detail, review-detail views

--- main/views.py
+++ main/views.py
@@ -6,3 +6,11 @@ class BookListView(ListView):
     model = models.Book
     template_name = 'main/books.html'
     context_object_name = 'books'
+
+class BookDetailView(DetailView):
+    model = models.Book
+    template_name = 'main/book.html'
+
+class ReviewDetail(DetailView):
+    model = models.Review
+    template_name = 'main/review.html'
