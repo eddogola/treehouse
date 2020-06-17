b5bcd2d3c5763e6dcd23aa3b347b9e7f16236709
Eddy Ogola <edogola01@gmail.com>
2020-06-17 17:59:53 +0300

    book model admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from sorl.thumbnail import get_thumbnail
 
-# Register your models here.
+from main import models
+
+@admin.register(models.Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('isbn', 'title', 'author',)
+
+    def book_cover(self, obj):
+        cover = get_thumbnail(obj.cover, '152x131')
+        return format_html(
+            "<img src='{}'>".format(cover.url)
+        )
