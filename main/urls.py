c4a4d1e850e5e7416781a140a1485ee5cfa45544
Eddy Ogola <edogola01@gmail.com>
2020-06-17 19:23:40 +0300

    urls for book & review detail views

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,6 @@ from main import views
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('books/', views.BookListView.as_view(), name='book_list'),
+    path('books/<uuid:pk>/', views.BookDetailView.as_view(), name='book_detail'),
+    path('reviews/<uuid:pk>/', views.ReviewDetail.as_view(), name='review_detail'),
 ]
