54b4625753d699520c01e0f79d83cb9dec28dc03
Eddy Ogola <edogola01@gmail.com>
2020-06-20 16:43:45 +0300

    urls for new views

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('books/', views.BookListView.as_view(), name='book_list'),
     path('books/<uuid:pk>/', views.BookDetailView.as_view(), name='book_detail'),
+    path('reviews/', views.ReviewList.as_view(), name='review_list'),
     path('reviews/<uuid:pk>/', views.ReviewDetail.as_view(), name='review_detail'),
+    path('books/discussions/', views.BookDiscussionList.as_view(), name='book_discussion_list'),
+    path('books/discussions/<uuid:pk>', views.BookDiscussionDetail.as_view(), name='book_discussion_detail'),
+    path('book-clubs/<uuid:pk>/', views.BookClubDetail.as_view(), name='book_club_detail'),
 ]
